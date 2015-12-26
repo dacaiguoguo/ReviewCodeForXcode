e9b70a39b9724037311ac774bf697077251c2d4d
dacaiguoguo <dacaiguoguo@163.com>
2015-12-26 21:46:29 +0800

    - (id)ivarOfKey:(NSString *)key

--- reviewcode/reviewcode.m
+++ reviewcode/reviewcode.m
@@ -124,12 +124,11 @@ id ivarOfObject(id obj, NSString* key) {
 }
 
 - (void)buttonClick:(id)sender {
-
-    id checkedFilePathsTokenTemp = ivarOfObject(self, @"_checkedFilePathsToken2");
-    id observedObjectTemp = ivarOfObject(checkedFilePathsTokenTemp, @"_observedObject");
-    NSArray *checkedFilePathsTemp = ivarOfObject(observedObjectTemp, @"_checkedFilePaths");
+    id checkedFilePathsTokenTemp = [self ivarOfKey:@"_checkedFilePathsToken2"];
+    id observedObjectTemp = [checkedFilePathsTokenTemp ivarOfKey:@"_observedObject"];
+    NSArray *checkedFilePathsTemp = [observedObjectTemp ivarOfKey:@"_checkedFilePaths"];
     [checkedFilePathsTemp enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSString *path = ivarOfObject(obj, @"_pathString");
+        NSString *path = [obj ivarOfKey:@"_pathString"];
         NSLog(@"%@",path);
     }];
 }
@@ -140,32 +139,18 @@ id ivarOfObject(id obj, NSString* key) {
 
 - (NSDictionary *)fromatDescription {
     NSMutableDictionary *dictionaryFormat = [NSMutableDictionary dictionary];
-    //  取得当前类类型
     Class cls = [self class];
-    
     unsigned int ivarsCnt = 0;
-    //　获取类成员变量列表，ivarsCnt为类成员数量
     Ivar *ivars = class_copyIvarList(cls, &ivarsCnt);
-    
-    //　遍历成员变量列表，其中每个变量都是Ivar类型的结构体
     for (const Ivar *p = ivars; p < ivars + ivarsCnt; ++p)
     {
         Ivar const ivar = *p;
-        
-        //　获取变量名
         NSString *key = [NSString stringWithUTF8String:ivar_getName(ivar)];
-        // 若此变量未在类结构体中声明而只声明为Property，则变量名加前缀 '_'下划线
-        // 比如 @property(retain) NSString *abc;则 key == _abc;
-        
-        //　获取变量值
         id value;
         @try {
             value = [self valueForKey:key];
         }@catch (NSException *exception) {}@finally {}
-        //　取得变量类型
-        // 通过 type[0]可以判断其具体的内置类型
-        if (value)
-        {
+        if (value) {
             [dictionaryFormat setObject:value forKey:key];
         }
     }
